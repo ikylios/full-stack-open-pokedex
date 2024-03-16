3656f9d73b33d61cb872d7d9d6df6c0dd9ed723b
xilxilx <ikylioss@gmail.com>
2024-03-16 11:34:25 +0200

    11.12

--- app.js
+++ app.js
@@ -11,3 +11,7 @@ app.listen(PORT, () => {
   /* eslint-disable-next-line */
   console.log(`server started on port ${PORT}`)
 })
+
+app.get('/health', (req, res) => {
+  res.status(200).send('ok')
+})
