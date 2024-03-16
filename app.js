be38f7675ae4c1e4a6f6dfde1f14931b0b1067a7
xilxilx <ikylioss@gmail.com>
2024-03-16 11:55:59 +0200

    11.12 fix maybe

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 /* eslint-env node */
-const express = require('express')
+const express = require("express")
 const app = express()
 
 // get the port from env variable
 const PORT = process.env.PORT || 5000
 
-app.use(express.static('dist'))
+app.use(express.static("dist"))
 
 app.listen(PORT, () => {
   /* eslint-disable-next-line */
   console.log(`server started on port ${PORT}`)
 })
 
-app.get('/health', (req, res) => {
-  res.status(400)
+app.get("/health", (req, res) => {
+  res.status(400).end()
   // res.status(200).send('ok')
 })
